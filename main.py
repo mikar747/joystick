5c67281827f25291fedd04e6f2dc18b480b31b9b
mikar747 <90870104+mikar747@users.noreply.github.com>
2022-12-14 22:39:14 +0100

    dodane radio

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ def my_function():
     basic.show_string("F")
 WSJoyStick.on_key(KEY.F, my_function)
 
+def on_received_number(receivedNumber):
+    if receivedNumber == 1:
+        MotorDriver.motor_run(Motor.A, Dir.FORWARD, 10)
+        MotorDriver.motor_run(Motor.B, Dir.FORWARD, 10)
+    if receivedNumber == 2:
+        MotorDriver.motor_run(Motor.A, Dir.BACKWARD, 10)
+        MotorDriver.motor_run(Motor.B, Dir.BACKWARD, 10)
+    if receivedNumber == 3:
+        MotorDriver.motor_run(Motor.A, Dir.FORWARD, 10)
+        MotorDriver.motor_run(Motor.B, Dir.BACKWARD, 10)
+    if receivedNumber == 4:
+        MotorDriver.motor_run(Motor.A, Dir.BACKWARD, 10)
+        MotorDriver.motor_run(Motor.B, Dir.FORWARD, 10)
+radio.on_received_number(on_received_number)
+
 def my_function2():
     basic.show_string("A")
 WSJoyStick.on_key(KEY.A, my_function2)
@@ -27,14 +42,24 @@ def my_function7():
 WSJoyStick.on_key(KEY.C, my_function7)
 
 WSJoyStick.joy_stick_init()
+radio.set_group(1)
 
 def on_forever():
-    if WSJoyStick.Listen_Dir(DIR.U):
-        basic.show_arrow(ArrowNames.NORTH)
-    if WSJoyStick.Listen_Dir(DIR.D):
-        basic.show_arrow(ArrowNames.SOUTH)
-    if WSJoyStick.Listen_Dir(DIR.L):
-        basic.show_arrow(ArrowNames.WEST)
-    if WSJoyStick.Listen_Dir(DIR.R):
-        basic.show_arrow(ArrowNames.EAST)
+    if WSJoyStick.Listen_Dir(DIR.NONE):
+        basic.clear_screen()
+        MotorDriver.motor_stop(Motor.A)
+        MotorDriver.motor_stop(Motor.B)
+    else:
+        if WSJoyStick.Listen_Dir(DIR.U):
+            basic.show_arrow(ArrowNames.NORTH)
+            radio.send_number(1)
+        if WSJoyStick.Listen_Dir(DIR.D):
+            basic.show_arrow(ArrowNames.SOUTH)
+            radio.send_number(2)
+        if WSJoyStick.Listen_Dir(DIR.L):
+            basic.show_arrow(ArrowNames.WEST)
+            radio.send_number(3)
+        if WSJoyStick.Listen_Dir(DIR.R):
+            basic.show_arrow(ArrowNames.EAST)
+            radio.send_number(4)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
 WSJoyStick.onKey(KEY.F, function () {
     basic.showString("F")
 })
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
+    }
+    if (receivedNumber == 2) {
+        MotorDriver.MotorRun(Motor.A, Dir.backward, 10)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, 10)
+    }
+    if (receivedNumber == 3) {
+        MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
+        MotorDriver.MotorRun(Motor.B, Dir.backward, 10)
+    }
+    if (receivedNumber == 4) {
+        MotorDriver.MotorRun(Motor.A, Dir.backward, 10)
+        MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
+    }
+})
 WSJoyStick.onKey(KEY.A, function () {
     basic.showString("A")
 })
@@ -19,6 +37,7 @@ WSJoyStick.onKey(KEY.B, function () {
 WSJoyStick.onKey(KEY.C, function () {
     basic.showString("C")
 })
+radio.setGroup(1)
 WSJoyStick.JoyStickInit()
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.NONE)) {
@@ -28,23 +47,19 @@ basic.forever(function () {
     } else {
         if (WSJoyStick.Listen_Dir(DIR.U)) {
             basic.showArrow(ArrowNames.North)
-            MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
-            MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
+            radio.sendNumber(1)
         }
         if (WSJoyStick.Listen_Dir(DIR.D)) {
             basic.showArrow(ArrowNames.South)
-            MotorDriver.MotorRun(Motor.A, Dir.backward, 10)
-            MotorDriver.MotorRun(Motor.B, Dir.backward, 10)
+            radio.sendNumber(2)
         }
         if (WSJoyStick.Listen_Dir(DIR.L)) {
             basic.showArrow(ArrowNames.West)
-            MotorDriver.MotorRun(Motor.A, Dir.forward, 10)
-            MotorDriver.MotorRun(Motor.B, Dir.backward, 10)
+            radio.sendNumber(3)
         }
         if (WSJoyStick.Listen_Dir(DIR.R)) {
             basic.showArrow(ArrowNames.East)
-            MotorDriver.MotorRun(Motor.A, Dir.backward, 10)
-            MotorDriver.MotorRun(Motor.B, Dir.forward, 10)
+            radio.sendNumber(4)
         }
     }
 })
